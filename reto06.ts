80d121244cab7815a5622bdb37e49d2517371591
Yerson Fabian Galvis <yersongalvis03@gmail.com>
2022-12-16 20:11:43 -0500

    challenge 06 done

---
+++ reto06.ts
@@ -0,0 +1,30 @@
+/*
+ * Reto #6
+ * INVIRTIENDO CADENAS
+ * Fecha publicación enunciado: 07/02/22
+ * Fecha publicación resolución: 14/02/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+const invertirCadena = (cadena: string):string => {
+    let split: string[] = cadena.split('');
+    let reverse: string[] = [];
+
+    for (let index:number = split.length-1; index >= 0; index--) {
+       reverse.push(split[index]);
+    }
+    const output:string = reverse.join('');
+    return output;
+}
+
+console.log(invertirCadena('Hola mundo'));
